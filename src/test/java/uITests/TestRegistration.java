8b0d6bbc5d962f3a28dc614183f88e34602eb815
Sataxe7 <129331004+Sataxe7@users.noreply.github.com>
2024-07-27 14:38:16 +0200

    Correct++ 27.07

--- src/test/java/uITests/TestRegistration.java
+++ src/test/java/uITests/TestRegistration.java
@@ -1,53 +1,36 @@
+
 package uITests;
 
-
-import driverManager.BaseTestHillel;
-import ui.hilel_site_obj.MainPage;
-
-import ui.malinator_service.MailinatorMainPage;
+import com.codeborne.selenide.Selenide;
+import driverManager.CommonMethods;
 import org.openqa.selenium.WindowType;
 import org.testng.annotations.Test;
+import ui.malinator_service.MailinatorMainPage;
 import ui.page_object.ConfirmEmailPage;
 import ui.page_object.ManePage;
-
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.switchTo;
-import static driverManager.CommonMethods.checkingContainsURL;
-import static driverManager.CommonMethods.randomMail;
-
-import static ui.page_object.PasswordPage.PASSWORD;
-import static ui.page_object.RecommendationsPage.recommendationsPageUrl;
-
+import ui.page_object.RecommendationsPage;
 
 public class TestRegistration extends BaseTestHilel {
-
-   ManePage manePage = new ManePage();
+    ManePage manePage = new ManePage();
     MailinatorMainPage mailinatorMainPage = new MailinatorMainPage();
     ConfirmEmailPage confirmEmailPage = new ConfirmEmailPage();
-
-    private String mail = randomMail();
-
-
+    private String mail = CommonMethods.randomMail();
     private String confirmCod;
+
+    public TestRegistration() {
+    }
 
     @Test
     public void testRegistration() {
-        System.out.println(mail);
-        open("https://ithillel.ua/");
-        manePage.clickByAuthPageBtn()
-                .checkFormLoginAndRegistrations()
-                .clickByRegistrationBtn()
-                .fillingRegistrationForm("Autotest", "Autotest", mail, "302346894")
-                .fillingPasswordInput(PASSWORD, PASSWORD);
-        switchTo().newWindow(WindowType.TAB);
-        switchTo().window(1);
-        open("https://www.mailinator.com");
-
-        confirmCod =  mailinatorMainPage.searchMail(mail)
-                .openLetterAndGetConfirmCode();
-        switchTo().window(0);
-        confirmEmailPage.fillingEmail(confirmCod)
-                .clickBySunmit();
-        checkingContainsURL(recommendationsPageUrl);
+        System.out.println(this.mail);
+        Selenide.open("https://ithillel.ua/");
+        this.manePage.clickByAuthPageBtn().checkFormLoginAndRegistrations().clickByRegistrationBtn().fillingRegistrationForm("Autotest", "Autotest").fillingPasswordInput("autotestHillel24", "autotestHillel24","063032312",mail);
+        Selenide.switchTo().newWindow(WindowType.TAB);
+        Selenide.switchTo().window(1);
+        Selenide.open("https://www.mailinator.com");
+        this.confirmCod = this.mailinatorMainPage.searchMail(this.mail).openLetterAndGetConfirmCode();
+        Selenide.switchTo().window(0);
+        this.confirmEmailPage.fillingEmail(this.confirmCod).clickBySunmit();
+        CommonMethods.checkingContainsURL(RecommendationsPage.recommendationsPageUrl);
     }
 }
