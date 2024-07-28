ad6957e30024b2b4ee9f563edab96f0607bf0612
Sataxe7 <129331004+Sataxe7@users.noreply.github.com>
2024-07-28 17:20:14 +0200

    Correct++ 28.07

--- src/test/java/uITests/TestRegistration.java
+++ src/test/java/uITests/TestRegistration.java
@@ -3,12 +3,16 @@ package uITests;
 
 import com.codeborne.selenide.Selenide;
 import driverManager.CommonMethods;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WindowType;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import ui.malinator_service.MailinatorMainPage;
 import ui.page_object.ConfirmEmailPage;
 import ui.page_object.ManePage;
 import ui.page_object.RecommendationsPage;
+
+import java.time.Duration;
 
 public class TestRegistration extends BaseTestHilel {
     ManePage manePage = new ManePage();
@@ -24,7 +28,7 @@ public class TestRegistration extends BaseTestHilel {
     public void testRegistration() {
         System.out.println(this.mail);
         Selenide.open("https://ithillel.ua/");
-        this.manePage.clickByAuthPageBtn().checkFormLoginAndRegistrations().clickByRegistrationBtn().fillingRegistrationForm("Autotest", "Autotest").fillingPasswordInput("autotestHillel24", "autotestHillel24","063032312",mail);
+        this.manePage.clickByAuthPageBtn().checkFormLoginAndRegistrations().clickByRegistrationBtn().fillingRegistrationForm("Autotest", "Autotest").fillingPasswordInput("autotestHillel24", "autotestHillel24", "063032312", this.mail);
         Selenide.switchTo().newWindow(WindowType.TAB);
         Selenide.switchTo().window(1);
         Selenide.open("https://www.mailinator.com");
